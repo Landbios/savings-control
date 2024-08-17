1596b5a4b25191e96c2997ff57f320a39274de1b
Land <76625399+Landbios@users.noreply.github.com>
2024-08-17 18:21:48 -0400

    budget validation and reducer creation

---
+++ src/components/BudgetForm.tsx
@@ -0,0 +1,26 @@
+import { useMemo, useState } from "react"
+
+const BudgetForm = () => {
+
+  const [budget,setBudget] = useState(0)
+
+  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+    setBudget(e.target.valueAsNumber)
+  }
+
+
+  //Validacion de presupuesto con useMemo
+  const isValid = useMemo(() => {return isNaN(budget) || budget ==0},[budget])
+  return (
+    <form action="" className="space-y-5">
+
+      <div className="flex flex-col space-y-5">
+        <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center"></label>
+        <input type="number" value={budget} onChange={(e) => handleChange(e)} name="budget" placeholder="define your budget" className="bg-white border border-gray-200 p-2" id="spends" />
+        <input type="submit" value="Define Budget" className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40 disabled:cursor-default" disabled={isValid}/>
+      </div>
+    </form>
+  )
+}
+
+export default BudgetForm
